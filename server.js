219cc535ca21f6e19f860e927103030aa0d896e7
Shikha Vyaghra <shikhavyaghra@Shikhas-MacBook-Air.local>
2022-11-21 10:18:17 -0500

    frontend to show collection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const path = require('path');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 const { response } = require("express");
 const { resolve } = require("path");
@@ -9,6 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+app.use(cors())
 const port = 3000;
 
 //Connection to Mysql
